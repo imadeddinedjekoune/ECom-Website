62889f37a4abf7e2890478fbe0a59232a94a771e
imadeddinedjekoune <imaddjekoune@gmail.com>
2023-09-10 12:19:00 -0700

    comm5

--- project/urls.py
+++ project/urls.py
@@ -19,11 +19,13 @@ from django.urls import path , include , re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.shortcuts import redirect
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/',include('store.urls')),
+    path('', lambda request: redirect('store/'), name='root_redirect'),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     re_path(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT})
 ] 
